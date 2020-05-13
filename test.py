a15f93aab27ed1f0f8053acd5e181a9391451015
Feifei Wang <feifei.wang@managebac.com>
2020-05-13 15:44:00 +0800

    过滤数据

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 import requests
 import json
 
-url = 'https://faria.openapply.cn/api/v1/students?auth_token=xxxxx'
+url = 'https://faria.devel-02.openapply.com//api/v1/students?auth_token=3e9249b6369374c0f9790f1d5a817d9b&per_page=10'
 response = requests.get(url)
 print(response) #<Response [200]>
 res = response.text
@@ -16,6 +16,14 @@ print(students)
 
 for s in students:
     print (s['name'],',',s['id'],',',s['email'],',',s['status'])
+
+for s in students:
+    if s['id'] == 7:
+        print(s)
+
+for s in students:
+    if s['first_name'] == 'Terry':
+        print(s['name'], s['id'], s['email'])
 
 
 
